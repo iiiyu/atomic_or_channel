5eab5f9827493ed882374c73d7c927491bc91ef9
Ewan Xiao <apple.iiiyu@gmail.com>
2022-08-09 23:31:38 +1200

    update ChannelAdd methods again

--- channel_add.go
+++ channel_add.go
@@ -1,21 +1,33 @@
 package main
 
-func ChannelAdd() uint64 {
-	one := uint64(1)
-	numCh := make(chan uint64, 1)
-	go func() {
-		for l := 0; l < LOOP*CLIENTS; l++ {
-			numCh <- one
-		}
-		close(numCh)
-	}()
-	return sum(numCh)
-}
+import (
+	"runtime"
+	"sync/atomic"
+)
 
-func sum(ch chan uint64) uint64 {
-	var sum uint64
-	for val := range ch {
-		sum += val
+func ChannelAdd() uint64 {
+	runtime.GOMAXPROCS(0)
+	var count uint64
+
+	// start worker
+	sum := func(ch chan *uint64) {
+		for value := range ch {
+			atomic.AddUint64(&count, *value)
+		}
 	}
-	return sum
+
+	one := uint64(1)
+	for i := 1; i <= CLIENTS; i++ {
+		ch := make(chan *uint64, LOOP)
+		go func() {
+			for l := 0; l < LOOP; l++ {
+				ch <- &one
+			}
+			close(ch)
+		}()
+		sum(ch)
+	}
+
+	return count
+
 }
